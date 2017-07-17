d56eebd3e521cf64319e8499b03a652f512d3b80
StefanoPoma97 <s.poma001@studenti.unibs.it>
2017-07-17 14:08:58 +0200

    prova

--- src/it/unibs/ing/fp/esame/Main.java
+++ src/it/unibs/ing/fp/esame/Main.java
@@ -10,18 +10,28 @@ public class Main {
 		int[][] mappa;
 		mappa= new int [3][3];
 		int k=0;
-		for (int i=0; i<3; i++)
-		{
-			for (int u=0; u<3; u++)
-			{
-				mappa[i][u]=k;
-				k++;
-			}
-		}
+		mappa[0][0]=3;
+		mappa[0][1]=1;
+		mappa[0][2]=4;
+		mappa[1][0]=0;
+		mappa[1][1]=1;
+		mappa[1][2]=0;
+		mappa[2][0]=3;
+		mappa[2][1]=2;
+		mappa[2][2]=5;
+		
+//		for (int i=0; i<3; i++)
+//		{
+//			for (int u=0; u<3; u++)
+//			{
+//				mappa[i][u]=k;
+//				k++;
+//			}
+//		}
 		Matrice m = new Matrice (mappa);
 		m.printMatrice();
 		m.dimensione();
-		System.out.println(m.laPlace());
+		System.out.println(m.determinante());
 		
 	}
 
